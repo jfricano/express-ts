9b6135a615df81292b183a10c65e3515aded52a2
Jason Fricano <44284799+jfricano@users.noreply.github.com>
2022-06-28 21:50:51 -0400

    add login route

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,4 +2,27 @@ import { Router, Request, Response } from 'express';
 
 export const router = Router();
 
-router.get('/', (req: Request, res: Response) => {});
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+        </div>
+        <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post(
+  '/login',
+  (req: Request<{}, { email: string; password: string }>, res: Response) => {
+    console.log(req);
+    const { email, password } = req.body;
+    res.send(email + password);
+  }
+);
