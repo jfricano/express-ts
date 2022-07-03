53d4124a34eab7e3491d8e82c2f2eeb0b6a8b52e
Jason Fricano <44284799+jfricano@users.noreply.github.com>
2022-07-03 19:16:33 -0400

    cleanup

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -14,15 +14,17 @@ class LoginController {
   @get('/login')
   // @use(logger)
   getLogin(_req: never, res: LoginResponse): void {
+    const { Email: email, Password: password } = KLoginBodyReq;
+
     res.send(`
       <form method="POST">
         <div>
           <label>Email</label>
-          <input name="email" />
+          <input name="${email}" type="${email}" />
           </div>
           <div>
           <label>Password</label>
-          <input name="password" type="password" />
+          <input name="${password}" type="${password}" />
         </div>
         <button>Submit</button>
       </form>
@@ -33,7 +35,11 @@ class LoginController {
   @bodyValidator(KLoginBodyReq.Email, KLoginBodyReq.Password)
   postLogin(req: LoginRequest, res: LoginResponse) {
     const { email, password } = req.body;
-    if (email === 'EHNUN' && password === 'Blueberry2022#') {
+
+    if (
+      email === 'jason.fricano.ext@bayer.com' &&
+      password === 'Blueberry2022#'
+    ) {
       req.session = { isLoggedIn: true };
       // res.redirect('/');
       res.status(302).send('you logged in!');
